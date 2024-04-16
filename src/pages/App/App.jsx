4a3288e1ca1386b588ceffacae84cc0609321065
jbwh <joelbawuah@hotmail.com>
2024-04-16 02:26:48 -0400

    two bonus' not complete

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,11 +2,9 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage'
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NotesListPage from '../NotesListPage/NotesListPage';
 import NavBar from '../../components/NavBar/NavBar';
 import { getUser } from "../../utilities/users-service";
-
 
 export default function App() {
   
@@ -18,8 +16,7 @@ export default function App() {
         <>
           <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/orders/new" element={ <NewOrderPage/> } />
-            <Route path="/orders" element={ <OrderHistoryPage/> } />
+            <Route path="/notes" element={ <NotesListPage/> } />
           </Routes>   
         </>
         :
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function NewOrdePage() {
-  return (
-    <h1>NewOrderPage</h1>
-  )
-}
-
